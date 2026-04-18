906a661e7082020de2f752b11e90695456776fc6
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2026-04-18 19:04:01 +0300

    sude naz karayıldırım

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
     return {
+      // 'repo-adi' kısmını GitHub'daki deponun tam adıyla değiştir (örn: /nexus-hud/)
+      base: '/skarayil/hud_life_cycle/', 
       define: {
-        'process.env.API_KEY' : JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
+        'process.env.API_KEY' : JSON.stringify('api-key-ignore'),
       },
       server: {
         proxy: {
